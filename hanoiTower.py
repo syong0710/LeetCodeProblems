5bfc4e93d792f85c517c3437d933faa1f4ab001b
SY <30416645+syong0710@users.noreply.github.com>
2022-08-25 15:12:20 -0700

    Update hanoiTower.py

--- hanoiTower.py
+++ hanoiTower.py
@@ -2,17 +2,13 @@
 # Tower of Hanoi: the recursive method
 def hanoi_recursive(n:int, source:str, auxiliary:str, destination:str):
     if n == 1:
-        print("Move disk 1 from source", source, "to destination", destination)
+        print("Move disk 1 from", source, "to", destination)
         return
     hanoi_recursive(n-1, source, destination, auxiliary) # move the (n-1) smaller disk to the auxiliary
-    print("Move disk", n, "from source", source, "to destination", destination) # move the biggest one to destination
+    print("Move disk", n, "from", source, "to", destination) # move the biggest one to destination
     hanoi_recursive(n-1, auxiliary, source, destination) #move the disks on the auxilary to the destiation
 
 
-
-#n = 4
-n = 3
-# A:source; B:destination; C:auxiliary;
+n = 4
+# A:source; B:auxiliary; C:destination;
 hanoi_recursive(n, 'A', 'B', 'C')
-
-
