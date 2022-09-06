2a66fa1559615c720a8ccb8f4616308f30f9d893
syong0710 <30416645+syong0710@users.noreply.github.com>
2022-09-06 11:29:12 -0700

    Update binarySearch.py

--- binarySearch.py
+++ binarySearch.py
@@ -1,23 +1,22 @@
-def binarySearch(inputArray, searchNum:int) ->int:
-    leftPointer:int = 0;
-    rightPointer:int = len(inputArray) -1
-    midPointer:int = leftPointer + round((rightPointer-leftPointer)/2)
+def binarySearch(inputArray, searchNum: int) -> int:
+    leftPointer: int = 0;
+    rightPointer: int = len(inputArray) - 1
 
     # pop put for the extreme cases
-    if searchNum<inputArray[leftPointer] or searchNum>inputArray[rightPointer]:
-        return []
+    if searchNum < inputArray[leftPointer] or searchNum > inputArray[rightPointer]:
+        return -1
 
     while leftPointer <= rightPointer:
         midPointer = leftPointer + round((rightPointer - leftPointer) / 2)
         if searchNum < inputArray[midPointer]:
-            rightPointer = midPointer
+            rightPointer = midPointer - 1
         if searchNum > inputArray[midPointer]:
-            leftPointer = midPointer+1
+            leftPointer = midPointer + 1
         if searchNum == inputArray[midPointer]:
             return midPointer
-    return []
+    return -1
+
 
 nums = [2, 3, 5, 13, 24, 90]
-searchNum = 90
-print("The index of the key number: "+str(binarySearch(nums,searchNum)))
-    
+searchNum = 13
+print("The index of the key number: " + str(binarySearch(nums, searchNum)))
