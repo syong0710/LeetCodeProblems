0f09b94e497012015f1c1b421764a3b2afa6f571
syong0710 <30416645+syong0710@users.noreply.github.com>
2022-09-06 16:26:09 -0700

    Update fib_sequ.py

--- fib_sequ.py
+++ fib_sequ.py
@@ -1,17 +1,10 @@
-# Fib sequence, the input is the number
 
-def fib(num:int):
-    # The initialization
-    if num==0:
-        return 0
-    if num==1:
-        return 1
+def fibSeq(n:int)->int:
+    if n>2:
+        return (fibSeq(n-1) + fibSeq(n-2))
+    else:
+        return n-1
 
-    # The change of status
-    return fib(num-1) + fib(num-2)
-
-
-n = 5
-print("Fib seq: " + str(fib(n)))
+print(fibSeq(6))
 
 
