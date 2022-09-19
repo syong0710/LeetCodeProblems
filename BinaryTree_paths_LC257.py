57ff26e0824fdc819f17f3c4612a161607e5b363
syong0710 <30416645+syong0710@users.noreply.github.com>
2022-09-18 19:01:01 -0700

    Update BinaryTree_paths_LC257.py

--- BinaryTree_paths_LC257.py
+++ BinaryTree_paths_LC257.py
@@ -1,66 +1,95 @@
 class TreeNode:
-    def __init__(self, nodeValue):
+    def __init__(self, nodeValue:int):
         self.value = nodeValue
         self.left = None
         self.right = None
 
 class BinaryTree:
-    def __init__(self, rootValue):
+    def __init__(self, rootValue:int):
         self.root = TreeNode(rootValue)
 
-    # preOrder traversal of the binary tree
-    def preOrderTrav(self, root:TreeNode)->list:
+    # in order traversal of the binayr tree
+    def inOrderTrav(self, root:TreeNode) -> list:
         if root is None:
             return []
-        result = []
-        self._preOrder(root,result)
-        return result
+        nodeList = []
+        self.inOrderRecur(root, nodeList)
+        return nodeList
 
-    def _preOrder(self, root: TreeNode, result:list):
-        if root is None:
-            return
-        else:
-            result.append(root.value)
-            self._preOrder(root.left, result)
-            self._preOrder(root.right, result)
-
-    # use the idea of preOrder traversal, get all the path of the tree
-    def binaryTreePaths(self, root:TreeNode)->list:
+    # level-order traversal of the tree:
+    def levelOrderTrav(self, root:TreeNode) -> list:
         if root is None:
             return []
+        nodeQue = []
+        nodeQue.append(root)
         result = []
-        path = ''
-        self._preOrderPath(root,path,result)
+        while nodeQue:
+            for _ in range(len(nodeQue)):
+                nodeTemp = nodeQue.pop(0)
+                result.append(nodeTemp.value)
+                if nodeTemp.left:
+                    nodeQue.append(nodeTemp.left)
+                if nodeTemp.right:
+                    nodeQue.append(nodeTemp.right)
         return result
 
+    # ALL PATH: pre-order traversal
+    def allPath(self, root:TreeNode)->list:
+        if root is None:
+            return []
+        path = []
+        result = []
+        self.preOrderAllPath(root, path, result)
+        return result
 
-    def _preOrderPath(self, root:TreeNode, path:str, result:list):
-        # Pre-order
-        path += str(root.value)
+    def preOrderAllPath(self, root: TreeNode, path: list, result:list):
+        path.append(root.value)
+        # When the node is leaf node we will update our result array
         if root.left is None and root.right is None:
-            result.append(path)
+            #result.append(path.copy())
+            strResult = ''
+            for i in range(len(path)-1):
+                strResult += str(path[i]) + '->'
+            strResult += str(path[-1])
+            result.append(strResult)
+            # backtracking of the path
+            del path[-1]
+            return
+        # recursively going left and right until we find the leaf and updating the array simultaneously
         if root.left:
-            self._preOrderPath(root.left, path+ " -> ", result)
+            self.preOrderAllPath(root.left, path, result)
         if root.right:
-            self._preOrderPath(root.right, path+ " -> ", result)
+            self.preOrderAllPath(root.right, path, result)
+        del path[-1]
 
 
 
+btree1 = BinaryTree(5)
+btree1.root.left = TreeNode(4)
+btree1.root.right = TreeNode(8)
+btree1.root.left.left = TreeNode(11)
+btree1.root.right.left = TreeNode(13)
+btree1.root.right.right = TreeNode(4)
+btree1.root.left.left.left = TreeNode(7)
+btree1.root.left.left.right = TreeNode(2)
+btree1.root.right.right.left = TreeNode(5)
+btree1.root.right.right.right = TreeNode(1)
+btree1.root.right.right.right = TreeNode(1)
+btree1.root.right.right.right = TreeNode(1)
+btree1.root.right.right.right.right = TreeNode(9)
 
-btree1 = BinaryTree(1)
-btree1.root.left = TreeNode(2)
-btree1.root.right = TreeNode(3)
-btree1.root.left.left = TreeNode(4)
-btree1.root.left.right = TreeNode(5)
 
 """
-            1
-        /      \
-       2        3
-     /  \
-    4    5
-
+               5
+          /           \
+        4              8
+       /              /   \
+     11              13    4     
+    /   \                 /  \  
+   7     2               5     1
+                                \
+                                 9
 """
 
-print("Pre-order traversal of the binary tree: " + str(btree1.preOrderTrav(btree1.root)))
-print("All paths of the binary tree: " + str(btree1.binaryTreePaths(btree1.root)))
+print("The level-order traversal of the binary tree: " + str(btree1.levelOrderTrav(btree1.root)))
+print("All the paths to the leaves: " + str(btree1.allPath(btree1.root)))
